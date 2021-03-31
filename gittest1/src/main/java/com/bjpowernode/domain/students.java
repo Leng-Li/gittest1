896c28d3b82e7baebc8c101f5d32bad60e137c49
leng <leng@614322108.com>
2021-03-31 16:48:28 +0800

    具体信息

--- gittest1/src/main/java/com/bjpowernode/domain/students.java
+++ gittest1/src/main/java/com/bjpowernode/domain/students.java
@@ -7,4 +7,10 @@ public class students {
     private Integer id;
     private String name;
     private String address;
+
+    public static void main(String[] args) {
+
+        String  name = "zhangsan";
+        Integer id = 100;
+    }
 }
